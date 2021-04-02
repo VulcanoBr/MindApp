abaaf3d297fa7b45f1498f2dd24d17f16695b3c3
VulcanoBr <vulcanorjbr@gmail.com>
2021-04-01 23:28:02 -0300

    Adiciona tela de Edit Content

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,13 +1,33 @@
 class ContentsController < ApplicationController
 
-  before_action :authenticate_user!
+    before_action :authenticate_user!
 
-  def index
-    @contents = current_user.contents
-  end
+    def index
+        @contents = current_user.contents
+    end
 
-  def new
-    @content = Content.new
-  end
-  
+    def new
+        @content = Content.new
+    end
+
+    def edit
+        @content = Content.find(params[:id])
+    end
+
+    def def create
+        @content = current_user.contents.build(content_params)
+        if @content.save
+            redirect_to contents_path, notice:  "content successfully created"
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def content_params
+        params.require(:content).permit(:title, :description)    
+    end
+    
+    
 end
