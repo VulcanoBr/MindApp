205ff92e39d288056ff60f03f7fa0cdefe00d3f9
VulcanoBr <vulcanorjbr@gmail.com>
2021-04-03 12:28:27 -0300

    Projeto MindApp

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,81 +1,75 @@
 class ContentsController < ApplicationController
-
     before_action :authenticate_user!
     before_action :set_content, only: [:show, :edit, :update, :destroy]
-
+  
     def index
-        @contents = current_user.contents
-
-        tag_names = params[:tags]
-
-        if tag_names.present?
-            @contents = @contents.joins(:tags).where(tags: { name: tag_names }).distinct
-        end
+      @contents = current_user.contents
+  
+      tag_names = params[:tags]
+  
+      if tag_names.present?
+        @contents = @contents.joins(:tags).where(tags: { name: tag_names }).distinct
+      end
     end
-
-    def new
-        @content = Content.new
-    end
-
+  
     def show
-        @markdown = Redcarpet::Markdown.new(Redcarpet::Render:HTML, autolink: true, tables: true)
-    end     
-
-    def edit
-        
+      @markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
     end
-
-    def def create
-        @content = current_user.contents.build(content_params)
-        if @content.save
-            associate_tags!
-            redirect_to contents_path, notice:  "content successfully created"
-        else
-            render 'new'
-        end
+  
+    def new
+      @content = Content.new
     end
-
+  
+    def create
+      @content = current_user.contents.build(content_params)
+  
+      if @content.save
+        associate_tags!
+  
+        redirect_to contents_path, notice: 'Content successfully created!'
+      else
+        render :new
+      end
+    end
+  
+    def edit; end
+  
     def update
-        
-        if @content.update(content_params)
-            associate_tags!
-            redirect_to content_path, notce: "content was successfully updated"
-        else
-            render 'edit'
-        end
+      if @content.update(content_params)
+        associate_tags!
+  
+        redirect_to contents_path, notice: 'Content successfully updated!'
+      else
+        render :edit
+      end
     end
-
+  
     def destroy
-        
-        if @content.destroy
-            redirect_to content_path, notice: 'content was successfully deleted.'
-        else
-            redirect_to contents_url
-        end
+      @content.destroy
+  
+      redirect_to contents_path, notice: 'Content successfully destroyed!'
     end
-    
-    
-
+  
     private
-
+  
     def set_content
-        @content = Content.find(params[:id])
+      @content = Content.find(params[:id])
     end
-
-    def tag_params
-        params.require(:content).permit(tags: [])[:tags].reject(&:blank?)
-    end 
-
+  
+    def tags_params
+      params.require(:content).permit(tags: [])[:tags].reject(&:blank?)
+    end
+  
     def content_params
-        params.require(:content).permit(:title, :description)    
+      params.require(:content).permit(:title, :description)
     end
-    
+  
     def associate_tags!
-        tags = tags_params.map do |tag_name|
-            current_user.tags.where(name: tag_name).first_or_initialize
-        end
-
-        @content.tags = tags
+      tags = tags_params.map do |tag_name|
+        current_user.tags.where(name: tag_name).first_or_initialize
+      end
+  
+      @content.tags = tags
     end
-    
+  
 end
